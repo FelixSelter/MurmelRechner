992558cdb841ddd58b49524951e672cb97b23517
FelixSelter <felix.selter@web.de>
2021-12-23 12:18:45 +0100

    Added actual syntax highlighting for assembly Added line numbers Added scrollpane

--- main.js
+++ main.js
@@ -1,20 +1,56 @@
+//on reload the textarea will still contain text. We have to put that into the code element and format the text
+function init() {
+    update(document.getElementById('editing').value);
+}
+window.onload = init;
+
 //Split screen from split.js
 Split(['#split-left', '#split-right']);
 
 function update(text) {
-    let result_element = document.querySelector('#highlighting-content');
     // Update code
-    result_element.innerText = text;
+    let code = document.getElementById('highlighting-content');
+    code.innerText = text;
+
     // Syntax Highlight
     highlight({
         patterns: [{
-                name: 'string',
-                match: /^(\'[^\'\n]*\')/,
+                name: 'format-register-number',
+                match: [/^inc ([0-9]+)/, '<span class="format-inc">inc </span>', ''],
             },
             {
-                name: 'fn-call',
-                match: [/^([A-z_]+)\(/, '', '('],
+                name: 'format-register-number',
+                match: [/^dec ([0-9]+)/, '<span class="format-dec">dec </span>', ''],
+            },
+            {
+                name: 'format-register-number',
+                match: [/^tst ([0-9]+)/, '<span class="format-tst">tst </span>', ''],
+            },
+            {
+                name: 'format-line-number',
+                match: [/^jmp ([0-9]+)/, '<span class="format-jmp">jmp </span> ', ''],
+            },
+            {
+                name: 'format-hlt',
+                match: /^(hlt)/,
             },
         ],
+        selector: '#highlighting-content',
     });
+
+    //linenums
+    let linenums = document.getElementById('linenums');
+    let linecount = text.split('\n').length;
+
+    //reset value
+    linenums.value = '';
+
+    //add line number for every line
+    for (let i = 1; i <= linecount; i++) linenums.value += i + '\n';
+
+    //adjust size if necessary
+    document.getElementById('scroll').style.gridTemplateColumns =
+        linecount.toString().length * 3.5 + '%';
+
+    document.getElementById('split').style.height = linenums.style.height;
 }
