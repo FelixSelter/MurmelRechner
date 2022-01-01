d9966a35e7929869f704133ea3a6c2f36bfc7c91
FelixSelter <felix.selter@web.de>
2022-01-01 19:42:13 +0100

    Fixed issue with opening the flowchart page due to typo

--- js/flowchart.js
+++ js/flowchart.js
@@ -1,5 +1,5 @@
 function generateFlowChart() {
-    let tab = window.open(windows.location + 'FlowChart.html', 'Flowchart');
+    let tab = window.open(window.location + 'FlowChart.html', 'Flowchart');
 
     tab.addEventListener(
         'load',
