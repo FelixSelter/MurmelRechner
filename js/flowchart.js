46ef497671bf0157097e2ce2f5f796d1318daa44
FelixSelter <felix.selter@web.de>
2022-01-01 20:11:50 +0100

    Fixed Github pages flowchart issue

--- js/flowchart.js
+++ js/flowchart.js
@@ -1,6 +1,6 @@
 function generateFlowChart() {
     let tab = window.open(
-        window.location.origin.replace('/index.html', '') + '/FlowChart.html',
+        window.location.href.replace('/index.html', '') + '/FlowChart.html',
         'Flowchart'
     );
 
