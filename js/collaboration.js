1bb148a55bf607314890aa8720b7d88eb6fcd0d1
FelixSelter <felix.selter@web.de>
2022-01-04 00:03:47 +0100

    Added confirm dialog when joining someone

--- js/collaboration.js
+++ js/collaboration.js
@@ -1,44 +1,27 @@
-var peer;
-
 function createCollaboration() {
-    //dont generate more than one
-    if (peer) return;
-
-    peer = new Peer();
-    peer.on('open', function(id) {
-        let e = document.getElementById('peerAdress');
-        e.text = peer.id;
-        e.style.border = '1px solid red';
-        e.href = window.location.href + '?host=' + peer.id;
+    collaborativeTextarea.init({
+        textarea: document.getElementById('editing'),
+        onOpen: enableRemote,
     });
+}
 
-    peer.on('connection', function(client) {
-        client.on('data', function(data) {
-            console.log(data);
-        });
-    });
+function enableRemote(id) {
+    //set the remote id
+    let e = document.getElementById('peerAdress');
+    e.text = id;
+    e.style.border = '1px solid red';
+    e.href = window.location.origin + '?host=' + id;
+
+    //clear the share button
+    document.getElementById('collaborationButton').outerHTML = '';
 }
 
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has('host')) {
     let hostID = urlParams.get('host');
 
-    //set the remote id
-    let e = document.getElementById('peerAdress');
-    e.text = hostID;
-    e.style.border = '1px solid red';
-    e.href = window.location.href + '?host=' + hostID;
-
-    //clear the share button
-    document.getElementById('collaborationButton').outerHTML = '';
-
-    //connect to the host
-    peer = new Peer();
-    peer.on('open', function(id) {
-        var host = peer.connect(hostID);
-        host.on('open', function() {
-            host.send('hi!');
-            console.log('connected');
-        });
-    });
+    const template = document.getElementById('joinDialog');
+    const clone = document.importNode(template.content, true);
+    document.body.appendChild(clone);
+    document.getElementById('questionBody').textContent = hostID;
 }
