e6a4686f6e9b3ae3d21da66efdc1ce876712687f
FelixSelter <felix.selter@web.de>
2022-01-05 18:04:58 +0100

    Fixed linenums width not working on ios

--- README.md
+++ README.md
@@ -19,7 +19,7 @@ https://felixselter.github.io/MurmelRechner/
 8. ![nextline](https://user-images.githubusercontent.com/55546882/148061731-c0e88d8e-d888-41f8-b9b3-b62d47ea682d.png)Execute this line. The current position is indicated by this pointer ![pointer](https://user-images.githubusercontent.com/55546882/148063270-7ed1955c-bf79-44c1-9df1-51b103d24272.png)
 9. ![collaboration](https://user-images.githubusercontent.com/55546882/148063904-1a50f737-1c97-4c0d-9cb0-a13fb65e0db7.png)Enable the collaboration mode
 10. ![fullscreen](https://user-images.githubusercontent.com/55546882/148063906-0b35c442-9ec7-43fc-bda9-a5f57d8bd66f.png)Enter the fullscreen mode
-11. ![id](https://user-images.githubusercontent.com/55546882/148065155-0ce3572b-c0e5-4694-99be-06e547b85215.png) Click you id to get a link so you can invite others
+11. ![id](https://user-images.githubusercontent.com/55546882/148065155-0ce3572b-c0e5-4694-99be-06e547b85215.png) Click the id to get a link so you can invite others
 
 ### Beautiful syntax highlighting
 ![syntax](https://user-images.githubusercontent.com/55546882/148062386-b810ba2e-40a4-4183-a949-d1f27a3521c5.png)
--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,6 @@
 //on reload the textarea will still contain text. We have to put that into the code element and format the text
 function init() {
-    update();
+  update();
 }
 window.onload = init;
 
@@ -14,120 +14,121 @@ window.onload = init;
 
 //Split screen from split.js
 Split(['#split-left', '#split-right'], {
-    onDrag: updateAllRegisters,
+  onDrag: updateAllRegisters,
 });
 
 function update() {
-    let text = document.getElementById('editing').value;
+  let text = document.getElementById('editing').value;
 
-    // Update code xss is handled as every single char is wrapped with a span
-    let code = document.getElementById('highlighting-content');
-    code.innerText = text;
+  // Update code xss is handled as every single char is wrapped with a span
+  let code = document.getElementById('highlighting-content');
+  code.innerText = text;
 
-    // Syntax Highlight
-    highlight({
-        patterns: [{
-                name: 'format-register-number',
-                match: [/^inc ([0-9]+)/, '<span class="format-inc">inc </span>', ''],
-            },
-            {
-                name: 'format-register-number',
-                match: [/^dec ([0-9]+)/, '<span class="format-dec">dec </span>', ''],
-            },
-            {
-                name: 'format-register-number',
-                match: [/^tst ([0-9]+)/, '<span class="format-tst">tst </span>', ''],
-            },
-            {
-                name: 'format-line-number',
-                match: [/^jmp ([0-9]+)/, '<span class="format-jmp">jmp </span> ', ''],
-            },
-            {
-                name: 'format-hlt',
-                match: /^(hlt)/,
-            },
-            {
-                name: 'format-none',
-                match: /^(&#10;)/,
-            },
-            {
-                name: 'format-none',
-                match: /^(-)/,
-            },
-            {
-                name: 'format-error',
-                match: /^(.)/,
-            },
-        ],
-        selector: '#highlighting-content',
-    });
+  // Syntax Highlight
+  highlight({
+    patterns: [
+      {
+        name: 'format-register-number',
+        match: [/^inc ([0-9]+)/, '<span class="format-inc">inc </span>', ''],
+      },
+      {
+        name: 'format-register-number',
+        match: [/^dec ([0-9]+)/, '<span class="format-dec">dec </span>', ''],
+      },
+      {
+        name: 'format-register-number',
+        match: [/^tst ([0-9]+)/, '<span class="format-tst">tst </span>', ''],
+      },
+      {
+        name: 'format-line-number',
+        match: [/^jmp ([0-9]+)/, '<span class="format-jmp">jmp </span> ', ''],
+      },
+      {
+        name: 'format-hlt',
+        match: /^(hlt)/,
+      },
+      {
+        name: 'format-none',
+        match: /^(&#10;)/,
+      },
+      {
+        name: 'format-none',
+        match: /^(-)/,
+      },
+      {
+        name: 'format-error',
+        match: /^(.)/,
+      },
+    ],
+    selector: '#highlighting-content',
+  });
 
-    //linenums
-    let linenums = document.getElementById('linenums');
-    let lines = text.split('\n');
+  //linenums
+  let linenums = document.getElementById('linenums');
+  let lines = text.split('\n');
 
-    //reset value
-    linenums.value = '';
+  //reset value
+  linenums.value = '';
 
-    //add line number for every line that is not empty
-    let counter = 1;
-    for (let i = 0; i < lines.length; i++) {
-        if (lines[i] != '') {
-            linenums.value += counter + '\n';
-            counter++;
-        } else {
-            linenums.value += '\n';
-        }
+  //add line number for every line that is not empty
+  let counter = 1;
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i] != '') {
+      linenums.value += counter + '\n';
+      counter++;
+    } else {
+      linenums.value += '\n';
     }
+  }
 
-    //adjust size if necessary
-    document.getElementById('scroll-right').style.gridTemplateColumns =
-        '40px ' + (getTextWidth(counter) + 20) + 'px';
+  //adjust size if necessary
+  document.getElementById('scroll-right').style.gridTemplateColumns =
+    '40px ' + (getTextWidth(counter - 1) + 25) + 'px';
 }
 
 function save() {
-    var text = document.getElementById('editing').value;
-    var blob = new Blob([text], { type: 'text/plain' });
-    var anchor = document.createElement('a');
-    anchor.download = 'code.txt';
-    anchor.href = window.URL.createObjectURL(blob);
-    anchor.target = '_blank';
-    anchor.style.display = 'none'; // just to be safe!
-    document.body.appendChild(anchor);
-    anchor.click();
-    document.body.removeChild(anchor);
+  var text = document.getElementById('editing').value;
+  var blob = new Blob([text], { type: 'text/plain' });
+  var anchor = document.createElement('a');
+  anchor.download = 'code.txt';
+  anchor.href = window.URL.createObjectURL(blob);
+  anchor.target = '_blank';
+  anchor.style.display = 'none'; // just to be safe!
+  document.body.appendChild(anchor);
+  anchor.click();
+  document.body.removeChild(anchor);
 }
 
 async function load(e) {
-    const input = e.target;
-    if (input.files && input.files.length > 0) {
-        const reader = new FileReader();
-        new Promise((resolve, reject) => {
-            reader.onload = (event) => resolve(event.target.result);
-            reader.onerror = (error) => reject(error);
-            reader.readAsText(input.files[0]);
-        }).then((data) => {
-            document.getElementById('editing').value = data;
-            update();
-        });
-    }
+  const input = e.target;
+  if (input.files && input.files.length > 0) {
+    const reader = new FileReader();
+    new Promise((resolve, reject) => {
+      reader.onload = (event) => resolve(event.target.result);
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(input.files[0]);
+    }).then((data) => {
+      document.getElementById('editing').value = data;
+      update();
+    });
+  }
 }
 document.getElementById('openButton').addEventListener('change', load);
 
 function getTextWidth(text) {
-    let measure = document.createElement('div');
-    measure.innerHTML = text;
-    measure.style.position = 'absolute';
-    measure.style.float = 'left';
-    measure.style.whiteSpace = 'nowrap';
-    measure.style.visibility = 'hidden';
-    measure.style.fontSize = '15pt';
-    measure.style.fontFamily = 'monospace';
-    measure.style.lineHeight = '20pt';
+  let measure = document.createElement('div');
+  measure.innerHTML = text;
+  measure.style.position = 'absolute';
+  measure.style.float = 'left';
+  measure.style.whiteSpace = 'nowrap';
+  measure.style.visibility = 'hidden';
+  measure.style.fontSize = '15pt';
+  measure.style.fontFamily = 'monospace';
+  measure.style.lineHeight = '20pt';
 
-    document.body.appendChild(measure);
-    let width = measure.clientWidth;
-    measure.remove();
+  document.body.appendChild(measure);
+  let width = measure.clientWidth;
+  measure.remove();
 
-    return width;
+  return width;
 }
